84cd3d5c6bb3121ce06e08eef3f30387d860e902
iMUZAHID <saddam@digi-techbd.com>
2016-04-06 15:39:43 +0600

    update swift 2.1 to 2.2

--- iMDropdownMenu/iMenu.swift
+++ iMDropdownMenu/iMenu.swift
@@ -8,18 +8,23 @@
 
 import UIKit
 
+
+class Color {
+	static let textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7)
+	static let placeHoderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.4)
+	static let themecolor = UIColor(red: 39/255, green: 175/255, blue: 15/255, alpha: 1)
+}
+
+
+
 protocol iMenuDelegate{
 	
 	func tapOnMenu(name: String, atIndex index: Int)
 	
 }
 
-
-let cell_background_color = UIColor.blueColor()
-let cell_text_color = UIColor.whiteColor()
-
 class iMenu: UIView {
-
+	
 	private var items:[UIButton] // Number of Cell in Menu
 	private var blur: UIView?  // Background make blur
 	private var superView:UIView? // the super view that hold the menu
@@ -29,7 +34,7 @@ class iMenu: UIView {
 	var delegate: iMenuDelegate?
 	
 	let k_item_height:CGFloat = 44.0 // Menu Cell height
-
+	
 	override init(frame: CGRect) {
 		self.items = [UIButton]()
 		myFrame = frame
@@ -43,7 +48,7 @@ class iMenu: UIView {
 	
 	func blurViewSetUp(){
 		blur = UIView()
-		let tap = UITapGestureRecognizer(target: self, action: "tapOnBackGround:")
+		let tap = UITapGestureRecognizer(target: self, action: #selector(iMenu.tapOnBackGround(_:)))
 		blur?.addGestureRecognizer(tap)
 		blur?.userInteractionEnabled = true
 		blur!.backgroundColor = UIColor.blackColor()
@@ -51,19 +56,12 @@ class iMenu: UIView {
 	}
 	
 	func tapOnBackGround(tap: UITapGestureRecognizer){
-		
 		self.hide()
 	}
 	
-	func addItem(name: String){
-		
-		
-		self.items.append(newItem(name))
-		
-		
-		reload()
-	}
-	
+	/**
+	Remove the previous items and added the new set of items
+	*/
 	func setItems(items:[String]){
 		self.items = []
 		for name in items{
@@ -72,52 +70,97 @@ class iMenu: UIView {
 		reload()
 	}
 	
+	/**
+	Add single item to the menu list
+	- parameters:
+	- name: Name to the new item to add
+	*/
+	func addItem(name: String){
+		self.items.append(newItem(name))
+		reload()
+	}
+	
+	/**
+	Add multiple items to the menu list.
+	- parameters:
+	- items: The array of string.
+	*/
 	func addItems(items:[String]){
 		for name in items{
 			
-				self.items.append(newItem(name))
+			self.items.append(newItem(name))
 		}
 		reload()
 	}
 	
-	func enableItemAtIndex(index: Int){
+	/**
+	- parameteres:
+	- index: The array of index of the item which becomes enable
+	*/
+	
+	func enableItemAtIndexes(indexes: [Int]){
 		
-		if index < items.count {
+		for index in indexes{
 			
-			self.items[index].enabled  = true
-			
-		}else{
-			
-			print("index out of range")
+			if index < items.count {
+				
+				self.items[index].enabled  = true
+				self.items[index].setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+				
+			}else{
+				print("index out of range")
+			}
 			
 		}
 		
 	}
 	
-	func disableItemAtIndex(index:Int){
+	/**
+	
+	- parameters:
+	- indexes: The array of index of the item which becomes disable
+	
+	*/
+	
+	func disableItemAtIndexes(indexes:[Int]){
 		
-		if index < items.count {
+		for index in indexes{
 			
-			self.items[index].enabled  = false
+			if index < items.count {
+				
+				self.items[index].enabled  = false
+				self.items[index].setTitleColor(Color.placeHoderColor, forState: UIControlState.Normal)
+				
+			}else{
+				print("index out of range")
+			}
 			
-		}else{
-			print("index out of range")
 		}
-
 		
 	}
+	
+	
+	/**
+	Create new item for the menu.
+	- parameters:
+	- name: Name of the new item for the menu.
+	*/
 	
 	private func newItem(name: String)->UIButton{
 		let newItem = UIButton()
 		newItem.frame = CGRectMake(0, 0, CGRectGetWidth(self.frame), k_item_height)
 		newItem.setTitle(name, forState: .Normal)
 		newItem.titleLabel?.font = UIFont.systemFontOfSize(20)
-		newItem.backgroundColor = cell_background_color
-		newItem.setTitleColor(cell_text_color, forState: .Normal)
+		newItem.backgroundColor = Color.themecolor
+		newItem.setTitleColor(UIColor.whiteColor(), forState: .Normal)
 		newItem.layer.borderWidth = 0.5
 		newItem.layer.borderColor = UIColor.blackColor().CGColor
 		return newItem
 	}
+	
+	/**
+	Re arrange the subviews after made any change
+	*/
 	
 	func reload(){
 		
@@ -134,20 +177,28 @@ class iMenu: UIView {
 		
 		var yPos:CGFloat = 0
 		
-		for(var i = 0; i<self.items.count; i++){
+		
+		for i in 0..<self.items.count{
 			items[i].frame.origin.y = yPos
 			items[i].tag = i
-			items[i].addTarget(self, action: "tapOnCell:", forControlEvents: UIControlEvents.TouchUpInside)
+			items[i].addTarget(self, action: #selector(iMenu.tapOnCell(_:)), forControlEvents: UIControlEvents.TouchUpInside)
 			self.addSubview(items[i])
 			yPos = yPos + k_item_height
 		}
-	
+		
 		if let blur = self.blur{
 			blur.frame = CGRectMake(CGRectGetMinX(blur.frame), CGRectGetHeight(self.myFrame), CGRectGetWidth(blur.frame), CGRectGetHeight(blur.frame) - CGRectGetHeight(self.myFrame))
 		}
 		
 	}
-
+	
+	/**
+	Super view of the menu item in which the subviews are added
+	
+	- parameters:
+	- view: The main view or container view under which the list become visiable.
+	*/
+	
 	func addToView(view: UIView){
 		
 		self.superView = view
@@ -155,8 +206,13 @@ class iMenu: UIView {
 		
 	}
 	
+	/**
+	The item which is tapped
+	- parameters:
+	- sender: UIButton in which is tapped
+	*/
 	func tapOnCell(sender: UIButton){
-	
+		
 		self.hide()
 		
 		if let del = self.delegate{
@@ -167,12 +223,14 @@ class iMenu: UIView {
 			}
 			
 		}
-	
+		
 	}
 	
-	
+	/**
+	Make visible the menu
+	*/
 	func show(){
-
+		
 		guard let mainView = self.superView else{
 			print("Super view did not set")
 			return
@@ -180,19 +238,23 @@ class iMenu: UIView {
 		
 		// already visible
 		if !isHide{
-		   return
+			return
 		}
 		
+		// if blur view does not added the main view
 		if !blur!.isDescendantOfView(mainView){
 			mainView.addSubview(blur!)
 			
 		}
+		
+		// if the menu container view does not added tha super view
 		if !self.isDescendantOfView(mainView){
 			mainView.addSubview(self)
 		}
 		
 		self.frame = CGRectMake(CGRectGetMinX(self.myFrame), CGRectGetMinY(self.myFrame)-CGRectGetHeight(self.myFrame), CGRectGetWidth(self.myFrame), CGRectGetHeight(self.myFrame))
 		
+		// menu make animated
 		[UIView .animateWithDuration(0.2, animations: { () -> Void in
 			self.frame = self.myFrame
 			}, completion: { (bool) -> Void in
@@ -202,6 +264,10 @@ class iMenu: UIView {
 		
 		
 	}
+	
+	/**
+	Hide the menu view
+	*/
 	
 	func hide(){
 		
@@ -220,12 +286,15 @@ class iMenu: UIView {
 		
 	}
 	required init?(coder aDecoder: NSCoder) {
-	//	super.init(coder: aDecoder)
+		//	super.init(coder: aDecoder)
 		fatalError("init(coder:) has not been implemented")
 	}
 	
 }
 
+/**
+View extension for shake animation
+*/
 extension UIView{
 	func shake(){
 		let animation = CABasicAnimation(keyPath: "position")
